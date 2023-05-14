06aaceb734516e4c84ebdabecd7314f4c01c6d75
Artur Jarominas <artur.jarominas@gmail.com>
2023-05-14 21:05:51 +0300

    home component 05.14.2023

--- react-landing-vacationhub/src/Components/Home/Home.jsx
+++ react-landing-vacationhub/src/Components/Home/Home.jsx
@@ -1,21 +1,30 @@
 import './home.css'
 import largeImg from './Home-Img-Large.png'
 import smallImg from './Home-Img-Small.png'
+
 const Home = () => {
     return (
         <section className='home'>
             <div className='home-content'>
                 <h1>
                     Dare to live <span className='life-word'>the life</span> you
-                    always <span className='wanted-word'>wanted</span>.
+                    always <p className='wanted-word'>wanted</p>
                 </h1>
                 <p>
                     Life is short and the world is wide. So, better get started.
                 </p>
             </div>
             <div className='home-images'>
-                <img src={largeImg} alt='home-img-large' />
-                <img src={smallImg} alt='home-img-small' />
+                <img
+                    className='img-large'
+                    src={largeImg}
+                    alt='home-img-large'
+                />
+                <img
+                    className='img-small'
+                    src={smallImg}
+                    alt='home-img-small'
+                />
             </div>
         </section>
     )
